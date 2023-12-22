b2baa35fd819fb539f5e106ccf450ba2b846524a
Jan Niklas Marzahl <janniklasmarzahl@gmail.com>
2023-12-22 16:12:34 +0100

    added revalidate tags

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,6 +3,8 @@ import { client, urlFor } from "@/app/lib/sanity";
 import { PortableText } from "@portabletext/react";
 import Image from "next/image";
 
+export const revalidate = 30; // revalidate at most every hour
+
 async function getData(slug: string) {
   const query = `
     *[_type == "blog" && slug.current == '${slug}'] {
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
+export const revalidate = 30; // revalidate at most 30 seconds
+
 async function getData() {
   const query = `
   *[_type == 'blog'] | order(_createdAt desc) {
